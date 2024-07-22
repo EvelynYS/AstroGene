ab5f1f56fe0b770e5ee954472eeab89246df1710
Evelyn Lai <evelynlaimofa@gmail.com>
2024-07-23 04:02:41 +0800

    revised

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -13,7 +13,7 @@ export const {
 
 const client = new sdk.Client();
 
-client.setEndpoint('https://cloud.appwrite.io/v1').setProject('66959c56002ff4e4efb6').setKey("bc70dbeca41da02edce8e7f02a37327dc3d3ced9f555f4ffc4a7c142479ecb22f6b0bd3231137a3d9dce892481d8d1fe8cd3256d83b28a3336d7395060fd7b2f7d77d340007dcc89122edfb5ea9619e01d0109c9cdf66b5812ed47c6ab95f39ef444a7b6f5fc53f9eae9e8cddbce121479c2d87091c466739aecdb8ac3cd18ae");
+client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
 export const databases = new sdk.Databases(client);
 export const users = new sdk.Users(client);
